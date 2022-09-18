0843fef392c32e14c6ed3653d6b7deb35586041d
Raoni Lourenco <raoni.lourenco@accern.com>
2022-09-17 21:56:02 -0400

    changing status of csv

--- cleanup.py
+++ cleanup.py
@@ -7,5 +7,11 @@ results += glob.glob('./*SIGMOD*/**/*pipeline_transform*', recursive=True)
 results += glob.glob('./*SIGMOD*/**/anchors.txt', recursive=True)
 
 
+results = glob.glob('./Examples/**/grptest.txt', recursive=True)
+results += glob.glob('./Examples/**/dp.txt', recursive=True)
+results += glob.glob('./Examples/**/*pipeline_transform*', recursive=True)
+results += glob.glob('./Examples**/anchors.txt', recursive=True)
+
+
 for f in results:
 	os.remove(f)
