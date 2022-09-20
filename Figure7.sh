6199b01db8035765012a4300df9129a930ad1d9e
Sainyam Galhotra <sainyamgalhotra@gmail.com>
2022-09-19 23:33:22 -0700

    fig 7 processing

---
+++ Figure7.sh
@@ -0,0 +1,16 @@
+#cd adult/
+#python Ouralgo_new.py
+#cd ../tweets
+#python Ouralgo.py
+#cd ../bmi
+#python Ouralgo.py
+#cd ../flights
+#python Ouralgo_new.py
+cd amazon
+python Ouralgo.py
+cd ../opendata
+python Ouralgo.py
+cd ../physicians
+python Ouralgo.py
+cd ../
+python read_output_fig7.py
---
+++ read_output_fig7.py
@@ -0,0 +1,36 @@
+import os
+
+time={'dp':{}, 'dp_nog':{}, 'dp_nb':{}}
+num_int={'dp':{}, 'bugdoc':{}, 'anchor':{}}
+for folder in ['tweets','adult','bmi','flights','amazon','opendata','physicians']:
+	if os.path.exists('./'+folder+'/dp.txt'):
+		f=open('./'+folder+'/dp.txt')
+		for line in f:
+			line=line.strip().split()
+			num_int['dp'][folder]=line[0]
+			time['dp'][folder]=line[1]
+		f.close()
+	else:
+		num_int['dp'][folder]=0
+		time['dp'][folder]='N/A'
+	if os.path.exists('./'+folder+'/nog.txt'):
+		f=open('./'+folder+'/nog.txt')
+		for line in f:
+			line=line.strip().split()
+			num_int['nog'][folder]=line[0]
+		f.close()
+	else:
+		num_int['nog'][folder]=0
+	if os.path.exists('./'+folder+'/nb.txt'):
+		f=open('./'+folder+'/nb.txt')
+		for line in f:
+			line=line.strip().split()
+			num_int['nb'][folder]=line[0]
+		f.close()
+	else:
+		num_int['nb'][folder]=0
+
+
+print (num_int)
+
+
